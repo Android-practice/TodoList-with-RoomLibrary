851b29d3d00f3bfeeda38ba9ef6a3a172d95328e
YujeongLEE <dldbwjd24@gmail.com>
2024-01-30 23:00:45 +0900

    add Room library in gradle.build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
 }
 
 android {
@@ -30,6 +31,10 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
+    viewBinding {
+        enable=true
+    }
 }
 
 dependencies {
@@ -41,4 +46,9 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+//    implementation("com.google.devtools.ksp:symbol-processing-api:1.6.21-1.0.5")
+
+    implementation("androidx.room:room-runtime:2.6.1")
+    kapt("androidx.room:room-compiler:2.6.1")
+
 }
